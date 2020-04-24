14352c0175ddc4a4b76e5287e4e33e80369287d3
Maneev Dumitru <54940217+ManeevDumitru@users.noreply.github.com>
2020-04-24 12:47:09 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
 class Slider {
   constructor() {
-    this.sliderContainer = document.getElementById('slider_wrapper');
+    this.sliderContainer = document.getElementById('slider_container');
+    this.sliderWrapper = document.getElementById('slider_wrapper');
     this.leftNav = document.getElementById('slide_left');
     this.rightNav = document.getElementById('slide_right');
-    this.sliders = document.getElementById('slider_wrapper');
     this.slide = document.getElementsByClassName('slide');
-    this.amountOfSlidesVisible = Math.round(this.sliderContainer.offsetWidth / this.slide[0].offsetWidth);
+    this.amountOfSlidesVisible = Math.round(this.sliderWrapper.offsetWidth / this.slide[0].offsetWidth);
     this.leftSlidesLeft = 0;
     this.rightSlidesLeft = Number(this.slide.length - this.amountOfSlidesVisible);
     this.position = 0;
-    this.test = 0;
+    this.leftOffset = 0;
   }
 
-  initializeSlider() {
-    let width = this.slide[0].offsetWidth;
-    for (let i = 1; i < this.slide.length; i++){
-      this.test += width;
-      console.log(this.test);
-      this.slide[i].style.left = this.test + 'px';
+  initializeSlider(amountOfSlides) {
+    let widthOfEachSlide = this.sliderContainer.offsetWidth / amountOfSlides;
+    this.sliderWrapper.style.width = this.slide.length * widthOfEachSlide + 'px';
+    for (let i = 0; i < this.slide.length; i++) {
+      this.slide[i].style.width = widthOfEachSlide + 'px';
+    }
+    for (let i = 1; i < this.slide.length; i++) {
+      this.leftOffset += widthOfEachSlide;
+      this.slide[i].style.left = this.leftOffset + 'px';
     }
   }
 
   sliderMovement(direction) {
     let width = this.slide[0].offsetWidth;
-    
     switch (direction) {
       case "left": 
         if (this.rightSlidesLeft > 0) {
           this.leftSlidesLeft++;
           this.rightSlidesLeft--;
           this.position += -width;
-          this.sliders.style.left = this.position + 'px';
+          this.sliderWrapper.style.left = this.position + 'px';
           console.log('Move left ' + this.position);
         } else {
           console.log('No slides to move');
@@ -41,7 +43,7 @@ class Slider {
           this.leftSlidesLeft--;
           this.rightSlidesLeft++;
           this.position += width;
-          this.sliders.style.left = this.position + 'px';
+          this.sliderWrapper.style.left = this.position + 'px';
           console.log('Move right ' + this.position);  
         } else {
           console.log('No slides to move');
@@ -54,8 +56,7 @@ class Slider {
 }
 
 const slider = new Slider();
-console.log(slider.amountOfVisibleSlides);
-slider.initializeSlider();
+slider.initializeSlider(6);
 slider.leftNav.addEventListener('click', () => {
   slider.sliderMovement('left');
 });
